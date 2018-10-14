2fd7d5c27f0bd2d179b2fa4498d7dfceae4c8c6a
Chris Ohk <utilForever@gmail.com>
2018-10-14 22:49:02 +0900

    Add CI configuration file for Travis CI

---
+++ Scripts/travis_build_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -eq 0 ]
+  then
+    docker build -t utilforever/hearthstoneppgui .
+    docker run utilforever/hearthstoneppgui
+else
+    docker build -f $1 -t utilforever/hearthstoneppgui:$2 .
+    docker run utilforever/hearthstoneppgui:$2 .
+fi
